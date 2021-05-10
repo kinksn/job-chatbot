94425e64b9a712ccd9c9ed89bad652ee41461856
shunya kataoka <shunya_kataoka@fringe81.com>
2021-05-10 18:43:33 +0900

    FIX

---
+++ src/firebase/config.js
@@ -0,0 +1,11 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyA6yWPWJsjrO9ommtniN2pf72Bv9ELAf-o",
+    authDomain: "chatbot-demo-6aca7.firebaseapp.com",
+    projectId: "chatbot-demo-6aca7",
+    storageBucket: "chatbot-demo-6aca7.appspot.com",
+    messagingSenderId: "540169060571",
+    appId: "1:540169060571:web:dda36fcaf2d5e8eb718cfe",
+    measurementId: "G-XF79ZN9R3N"
+};
+
+export default firebaseConfig
